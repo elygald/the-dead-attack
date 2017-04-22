9b8dac93c7189923c63a2eab287e3802aecf5e16
Ely Galdino <elygald@gmail.com>
2017-04-22 16:41:34 -0300

    create menu

---
+++ Assets/testes/scripts/Pause.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject menu;
+
+	void start(){
+		menu.GetComponent<MeshRenderer>().enabled = false;
+		menu.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			menu.SetActive (true);
+			Time.timeScale = 0f;
+		}
+	
+	}
+	public void voltar(){
+		Time.timeScale = 1f;
+		menu.SetActive (false);
+	}
+}
---
+++ Assets/testes/scripts/menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	public Button startgame;
+	public Button exitgame;
+	// Use this for initialization
+	void Start () {
+		Button exit = exitgame.GetComponent<Button> ();
+		exit.onClick.AddListener (Exit_game);
+		Button btn = startgame.GetComponent<Button> ();
+		btn.onClick.AddListener (Start_Game);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();
+	}
+	void Start_Game(){
+		Application.LoadLevel ("demo");
+	}
+	void Exit_game(){
+		Application.Quit();
+	}
+}
